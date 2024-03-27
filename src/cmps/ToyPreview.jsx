c822b3914f2d4bbc8da0db2c56a77008e3b9a948
Ben <bnk989@gmail.com>
2024-03-27 08:24:38 +0200

    improve toy preview css

--- src/cmps/ToyPreview.jsx
+++ src/cmps/ToyPreview.jsx
@@ -4,15 +4,17 @@ import { Link } from "react-router-dom";
 
 export function ToyPreview({ toy }) {
     return (
-        <article>
+        <article className="flex">
+            <span className="stockStatus">{toy.inStock ? 'In Stock' : 'Out of Stock'}</span>
             <h4>{toy.title}</h4>
             <p>Price: <span>${toy.price.toLocaleString()}</span></p>
             <ul className="labels clean-list flex">{toy.labels.map((label) => <li key={label}>{label}</li>)}</ul>
             {toy.owner && <p>Owner: <Link to={`/user/${toy.owner._id}`}>{toy.owner.fullname}</Link></p>}
             <hr />
-            <Link to={`/toy/edit/${toy._id}`}>Edit</Link> &nbsp; | &nbsp;
-            <Link to={`/toy/${toy._id}`}>Details</Link>
-
+            <div className="actions">
+                <Link to={`/toy/edit/${toy._id}`}>Edit</Link> &nbsp; | &nbsp;
+                <Link to={`/toy/${toy._id}`}>Details</Link>
+            </div>
         </article>
     )
 }
